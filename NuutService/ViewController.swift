ee319c73e16d340564fbd9eceec7e25c11df493d
Supasit.s <asd@com.com>
2019-01-15 13:51:37 +0700

    Initial Commit

---
+++ NuutService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NuutService
+//
+//  Created by TST-APP-01 on 15/1/2562 BE.
+//  Copyright © 2562 Nuutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
