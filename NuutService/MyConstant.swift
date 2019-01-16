d420c84c7b5a447b136b995057c2d9215cd30128
Supasit.s <asd@com.com>
2019-01-16 14:16:34 +0700

    ตอนที่ 11 สามารถ GET URL เรียบร้อยแล้ว

--- NuutService/MyConstant.swift
+++ NuutService/MyConstant.swift
@@ -10,8 +10,15 @@ import Foundation
 
 class MyConstant {
     let urlAddUser:String = "https://www.androidthai.in.th/hit/addUserNut.php?isAdd=true&Name="
+    let urlGetUser:String = "https://www.androidthai.in.th/hit/getUserWhereUserNut.php?isAdd=true&User="
+    
     
     func getUrlAddUser() -> String {
         return urlAddUser
+    }
+    
+    func getUrlGetUser(user:String) -> String {
+        let result = "\(urlGetUser)\(user)"
+        return result
     }
 }
--- NuutService/ViewController.swift
+++ NuutService/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    Explicit
+    var user:String?
+    var password:String?
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func myAlert(title:String,message:String) -> Void {
+        let objAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        objAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            objAlert.dismiss(animated: true, completion: nil)
+        }))
+        present(objAlert, animated: true, completion: nil)
+        
+    }
+    
+    func checkAuthen(user:String,password:String) -> Void {
+        let myConstant = MyConstant()
+        let urlString:String = myConstant.getUrlGetUser(user: user)
+        print("urlString ==> \(urlString)")
+        
+        
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        user = userTextField.text
+        password = passwordTextField.text
+        
+        if (user!.count == 0) || (password!.count == 0) {
+            myAlert(title: "Have Space", message: "Please Fill All Blank")
+            print("HAVE SPACE")
+//            HAVE SPACE
+        }else{
+            print("NO SPACE")
+//            NO SPACE
+            checkAuthen(user: user!, password: password!)
+        }
+        
+        
+        
+    }
+    
 }
 
